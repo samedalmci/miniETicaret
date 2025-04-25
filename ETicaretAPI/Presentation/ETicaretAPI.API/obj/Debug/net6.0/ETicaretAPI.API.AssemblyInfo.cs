3d1a9c90727338dcb87a26f59e87b40390ba9b2c
Melik Samed ALMACI <almaci66@gmail.com>
2025-04-25 15:49:03 +0300

    25V1

--- ETicaretAPI/Presentation/ETicaretAPI.API/obj/Debug/net6.0/ETicaretAPI.API.AssemblyInfo.cs
+++ ETicaretAPI/Presentation/ETicaretAPI.API/obj/Debug/net6.0/ETicaretAPI.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ETicaretAPI.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+071d19347ba472708df3064bfafc3128e8fee6ae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51482715cc5cc2438b7833f50eacc759f032cb70")]
 [assembly: System.Reflection.AssemblyProductAttribute("ETicaretAPI.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ETicaretAPI.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

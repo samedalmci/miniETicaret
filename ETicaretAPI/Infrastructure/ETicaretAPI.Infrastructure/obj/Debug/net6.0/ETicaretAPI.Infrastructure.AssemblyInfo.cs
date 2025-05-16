b3280014f713929c8bd6c916462bbc151d712574
Melik Samed ALMACI <almaci66@gmail.com>
2025-05-16 16:31:40 +0300

    W37

--- ETicaretAPI/Infrastructure/ETicaretAPI.Infrastructure/obj/Debug/net6.0/ETicaretAPI.Infrastructure.AssemblyInfo.cs
+++ ETicaretAPI/Infrastructure/ETicaretAPI.Infrastructure/obj/Debug/net6.0/ETicaretAPI.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ETicaretAPI.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3187064048c14c2a77246fed5d438e4c94021bcb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d82eb0c20a055038581b0b228df4586a2260c7be")]
 [assembly: System.Reflection.AssemblyProductAttribute("ETicaretAPI.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ETicaretAPI.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
